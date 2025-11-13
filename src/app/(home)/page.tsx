e65eed6933d5cfefb3348c1e0914d880d2ea5275
Nicolas Angelo <nick.angelo.scott@gmail.com>
2025-11-13 15:48:09 -0500

    fix homepage

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,23 @@
+import Link from 'next/link'
+import { SignedIn, SignedOut } from '@clerk/nextjs'
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
-  return <main className="flex flex-col items-center justify-center">hi</main>
+  return (
+    <main className="flex flex-col items-center justify-center space-y-4">
+      <h1 className="text-center font-semibold text-xl">
+        AI SaaS w/ API Keys Quickstart
+      </h1>
+      <SignedIn>
+        <Button asChild>
+          <Link href="/dashboard">Go to Dashboard</Link>
+        </Button>
+      </SignedIn>
+      <SignedOut>
+        <Button asChild>
+          <Link href="/sign-in">Sign In</Link>
+        </Button>
+      </SignedOut>
+    </main>
+  )
 }
